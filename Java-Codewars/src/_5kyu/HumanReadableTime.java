384d498dfc947560f3da932a66da36958a6417e6
iantato <margiehouses@gmail.com>
2023-11-20 21:41:10 +0800

    added solved 5kyu

---
+++ Java-Codewars/src/_5kyu/HumanReadableTime.java
@@ -0,0 +1,31 @@
+package _5kyu;
+
+/*
+ * 
+ * Challenge: (5kyu) Human Readable Time
+ * Details:
+ *  
+ *  Write a function, which takes a non-negative integer (seconds) as input and returns
+ *  the time in a human-readable format (HH:MM:SS).
+ * 
+ *  - HH = hours, padded to 2 digits, range: 00-99
+ *  - MM = minutes, padded to 2 digits, range: 00-59
+ *  - SS = seconds, padded to 2 digits, range: 00-59
+ * 
+ *  The maximum time never exceeds 359999 (99:59:59)
+ *  
+ *  You can find some examples in the test fixtures.
+ * 
+ */
+
+public class HumanReadableTime {
+    
+    public static String makeReadable(int seconds) {
+        int hour = (seconds / 60) / 60,
+            min = (seconds - (hour * 60 * 60)) / 60,
+            sec = seconds - (min * 60) - (hour * 60 * 60);
+
+        return String.format("%02d:%02d:%02d", hour, min, sec);
+    }
+
+}
